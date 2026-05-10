df35427c978a2a7055940d39bb099b454ff09a16
Sudip Das <sudip5428@gmail.com>
2026-05-10 08:39:39 +0530

    code commit for change category routing and read.md file

--- backend/src/routes/categoryRoute.ts
+++ backend/src/routes/categoryRoute.ts
@@ -7,12 +7,13 @@ import {
   remove,
 } from "../controllers/categoryController";
 import { authMiddleware } from "../middleware/authMiddleware";
+import { requiredRole } from "../middleware/roleMiddleware";
 
 const categoryRouter = Router();
 categoryRouter.get("/", get);
-categoryRouter.post("/", authMiddleware, create);
+categoryRouter.post("/", authMiddleware, requiredRole("admin"), create);
 categoryRouter.get("/:id", authMiddleware, find);
-categoryRouter.put("/:id", authMiddleware, put);
-categoryRouter.delete("/:id", authMiddleware, remove);
+categoryRouter.put("/:id", authMiddleware, requiredRole("admin"), put);
+categoryRouter.delete("/:id", authMiddleware, requiredRole("admin"), remove);
 
 export default categoryRouter;
